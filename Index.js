a15a0c2811588038c6d857c546aeeb42fcbe906b
Md Mahtab Ali <161712272+hrmahtab01@users.noreply.github.com>
2024-10-22 01:41:40 +0600

    dynamic route done

--- Index.js
+++ Index.js
@@ -1,12 +1,20 @@
-const { log } = require("console");
 const http = require("http");
 const url = require("url");
+const fs = require("fs");
+const port = 8080;
 
-http
-  .createServer(function (req, res) {
-    res.write("Hello World!");
-    res.end();
-  })
-  .listen(8080);
-  console.log(url);
-  
+const myserver = http.createServer((req, res) => {
+  let pathaddr = url.parse(req.url, true);
+  let filename = "." + pathaddr.pathname;
+  fs.readFile(`${filename}.html`, (errr, result) => {
+   if (errr) {
+    res.end("404 not found")
+   }else{
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(result);
+    return res.end();
+   }
+  });
+});
+
+myserver.listen(port);
